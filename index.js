7e86289900dba59ed7461472ab6f05878b53c61d
Jonathan Neal <jonathantneal@hotmail.com>
2016-02-15 14:05:43 -0500

    Update references

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-var fs  = require('fs');
+var fs = require('fs');
+var cssnext = require('postcss-cssnext');
 
-require('postcss-cssnext')({
-	features: {
-		autoprefixer: {
-			remove: false
-		}
-	}
-}).process(fs.readFileSync('sanitize.css', 'utf8'), {
-	from: 'sanitize.css',
-	to:   'dist/sanitize.css',
-	map:  {
-		inline: false
-	},
-}).then(function (result) {
-	fs.writeFileSync('dist/sanitize.css', result.css);
-	fs.writeFileSync('dist/sanitize.css.map', result.map);
+var enc  = 'utf8';
 
-	require('cssnano')().process(result.css, {
-		from: 'dist/sanitize.css',
-		to:   'dist/sanitize.min.css',
-		map:  {
-			inline: false
-		},
-	}).then(function (result) {
-		fs.writeFileSync('dist/sanitize.min.css', result.css);
-		fs.writeFileSync('dist/sanitize.min.css.map', result.map);
-	});
+var opts = {
+	from: 'lib/sanitize.css',
+	to:   'dist/sanitize.css'
+};
+
+cssnext.process(fs.readFileSync(opts.from, enc), opts).then(function (result) {
+	fs.writeFileSync(opts.to, result.css, enc);
 });
